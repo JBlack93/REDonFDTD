83839122ea18f70669803499591e94c3d3d73637
James Black <james.a.black@durham.ac.uk>
2020-03-01 17:03:46 +0000

    Add Regression test for project output

---
+++ t/regression.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "1 1 1" | ./REDonFDTD
+
+diff dimensions2.txt $1/$2
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+diff ExXZ.txt $1/$3
+if [[ $? != 0 ]]; then
+    exit 2
+fi
+
+rm -f dimensions2.txt
+rm -f ExXZ.txt
+
+exit 0
