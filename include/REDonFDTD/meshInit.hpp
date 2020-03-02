6b7b425c3bb599d86fa8d192764a52138aec528e
James Black <james.a.black@durham.ac.uk>
2020-03-02 15:50:50 +0000

    Create Mesh class constructor and destructor

--- include/REDonFDTD/meshInit.hpp
+++ include/REDonFDTD/meshInit.hpp
@@ -1,18 +1,136 @@
 /** @file meshInit.hpp
  *  @brief Contains the Mesh struct */
 #pragma once
+
+#include <cmath>
+#include "REDonFDTD/memAllocation.hpp"
+
 //! @brief Mesh is the 4D space-time grid which contains both E and H fields.
-struct Mesh
+class Mesh
 {
-    double *hx, *chxh, *chxe;              //!< H_x field. Plus coefficients needed in its Update equation
-    double *hy, *chyh, *chye;              //!< H_y field. Plus coefficients needed in its Update equation
-    double *hz, *chzh, *chze;              //!< H_z field. Plus coefficients needed in its Update equation
-    double *ex, *cexe, *cexh;              //!< E_x field. Plus coefficients needed in its Update equation
-    double *ey, *ceye, *ceyh;              //!< E_y field. Plus coefficients needed in its Update equation
-    double *ez, *ceze, *cezh;              //!< E_z field. Plus coefficients needed in its Update equation
-    int sizeX, sizeY, sizeZ;               //!<  Dimensions of the Mesh
-    long double time, timeStep, maxTime;   //!< Define advance in time.
-    double cdtds;                          //!< Courant number (Maximal distance which can be traveled in a timestep)
+public:
+  Mesh(){
+    double imp0 = 377.0;
+    int mm, nn, pp;
+    this->sizeX = 51;                       // size of domain
+    this->sizeY = 50;
+    this->sizeZ = 50;
+    this->timeStep = 5*pow(10,-10);
+    this->maxTime = 100*this->timeStep;           // duration of simulation
+    this->cdtds = 1.0 / sqrt(3.0);          // Courant number
+
+    /* memory allocation */
+    this->hx = ALLOC_3D(this->hx,   this->sizeX,     this->sizeY - 1, this->sizeZ - 1);
+    this->chxh = ALLOC_3D(this->chxh, this->sizeX,     this->sizeY - 1, this->sizeZ - 1);
+    this->chxe = ALLOC_3D(this->chxe, this->sizeX,     this->sizeY - 1, this->sizeZ - 1);
+    this->hy = ALLOC_3D(this->hy,   this->sizeX - 1, this->sizeY,     this->sizeZ - 1);
+    this->chyh = ALLOC_3D(this->chyh, this->sizeX - 1, this->sizeY,     this->sizeZ - 1);
+    this->chye = ALLOC_3D(this->chye, this->sizeX - 1, this->sizeY,     this->sizeZ - 1);
+    this->hz = ALLOC_3D(this->hz,   this->sizeX - 1, this->sizeY - 1, this->sizeZ);
+    this->chzh = ALLOC_3D(this->chzh, this->sizeX - 1, this->sizeY - 1, this->sizeZ);
+    this->chze = ALLOC_3D(this->chze, this->sizeX - 1, this->sizeY - 1, this->sizeZ);
+
+
+    this->ex = ALLOC_3D(this->ex,   this->sizeX - 1, this->sizeY,     this->sizeZ);
+    this->cexe = ALLOC_3D(this->cexe, this->sizeX - 1, this->sizeY,     this->sizeZ);
+    this->cexh = ALLOC_3D(this->cexh, this->sizeX - 1, this->sizeY,     this->sizeZ);
+    this->ey = ALLOC_3D(this->ey,   this->sizeX,     this->sizeY - 1, this->sizeZ);
+    this->ceye = ALLOC_3D(this->ceye, this->sizeX,     this->sizeY - 1, this->sizeZ);
+    this->ceyh = ALLOC_3D(this->ceyh, this->sizeX,     this->sizeY - 1, this->sizeZ);
+    this->ez = ALLOC_3D(this->ez,   this->sizeX,     this->sizeY,     this->sizeZ - 1);
+    this->ceze = ALLOC_3D(this->ceze, this->sizeX,     this->sizeY,     this->sizeZ - 1);
+    this->cezh = ALLOC_3D(this->cezh, this->sizeX,     this->sizeY,     this->sizeZ - 1);
+
+
+    /* set electric-field update coefficients */
+    for (mm = 0; mm < this->sizeX - 1; ++mm)
+    {
+      for (nn = 0; nn < this->sizeY; ++nn)
+      {
+        for (pp = 0; pp < this->sizeZ; ++pp)
+        {
+          this->cexe[(mm * (this->sizeY) + nn) * this->sizeZ + pp] = 1.0;
+          this->cexh[(mm * (this->sizeY) + nn) * this->sizeZ + pp] = this->cdtds * imp0;
+        }
+      }
+    }
+
+    for (mm = 0; mm < this->sizeX; mm++)
+    {
+      for (nn = 0; nn < this->sizeY - 1; ++nn)
+      {
+        for (pp = 0; pp < this->sizeZ; ++pp)
+        {
+          this->ceye[(mm*(this->sizeY - 1) + nn) * this->sizeZ + pp] = 1.0;
+          this->ceyh[(mm*(this->sizeY - 1) + nn) * this->sizeZ + pp] = this->cdtds * imp0;
+        }
+      }
+    }
+
+
+    for (mm = 0; mm < this->sizeX; ++mm)
+    {
+      for (nn = 0; nn < this->sizeY; ++nn)
+      {
+        for (pp = 0; pp < this->sizeZ - 1; ++pp)
+        {
+          this->ceze[(mm * (this->sizeY) + nn) * (this->sizeZ - 1) + pp] = 1.0;
+          this->cezh[(mm * (this->sizeY) + nn) * (this->sizeZ - 1) + pp] = this->cdtds * imp0;
+        }
+      }
+    }
+
+    /* set magnetic-field update coefficients */
+    for (mm = 0; mm < this->sizeX; ++mm)
+    {
+      for (nn = 0; nn < this->sizeY - 1; ++nn)
+      {
+        for (pp = 0; pp < this->sizeZ - 1; ++pp)
+        {
+          this->chxh[(mm * (this->sizeY - 1) + nn) * (this->sizeZ - 1) + pp] = 1.0;
+          this->chxe[(mm * (this->sizeY - 1) + nn) * (this->sizeZ - 1) + pp] = this->cdtds / imp0;
+        }
+      }
+    }
+
+    for (mm = 0; mm < this->sizeX - 1; ++mm)
+    {
+      for (nn = 0; nn < this->sizeY; ++nn)
+      {
+        for (pp = 0; pp < this->sizeZ - 1; ++pp)
+        {
+          this->chyh[(mm * this->sizeY + nn) * (this->sizeZ - 1) + pp] = 1.0;
+          this->chye[(mm * this->sizeY + nn) * (this->sizeZ - 1) + pp] = this->cdtds / imp0;
+        }
+      }
+    }
+
+    for (mm = 0; mm < this->sizeX - 1; ++mm)
+    {
+      for (nn = 0; nn < this->sizeY - 1; ++nn)
+      {
+        for (pp = 0; pp < this->sizeZ; ++pp)
+        {
+          this->chzh[(mm * (this->sizeY - 1) + nn) * this->sizeZ + pp] = 1.0;
+          this->chze[(mm * (this->sizeY - 1) + nn) * this->sizeZ + pp] = this->cdtds / imp0;
+        }
+      }
+    }
+
+    return;
+  }          /* end Mesh() */
+
+  ~Mesh() = default;
+
+  double *hx, *chxh, *chxe;              //!< H_x field. Plus coefficients needed in its Update equation
+  double *hy, *chyh, *chye;              //!< H_y field. Plus coefficients needed in its Update equation
+  double *hz, *chzh, *chze;              //!< H_z field. Plus coefficients needed in its Update equation
+  double *ex, *cexe, *cexh;              //!< E_x field. Plus coefficients needed in its Update equation
+  double *ey, *ceye, *ceyh;              //!< E_y field. Plus coefficients needed in its Update equation
+  double *ez, *ceze, *cezh;              //!< E_z field. Plus coefficients needed in its Update equation
+  int sizeX, sizeY, sizeZ;               //!<  Dimensions of the Mesh
+  long double time, timeStep, maxTime;   //!< Define advance in time.
+  double cdtds;                          //!< Courant number (Maximal distance which can be traveled in a timestep)
 };
 
-typedef struct Mesh Mesh;
+// typedef class Mesh Mesh;
--- src/bin/REDonFDTD.cpp
+++ src/bin/REDonFDTD.cpp
@@ -8,28 +8,26 @@
 
 int main()
 {
-    Mesh *g;
-    Particle *p;
+  Mesh * g = new Mesh;
+  Particle *p;
 
-    g = ALLOC_1D(g, 1);       // allocate memory for mesh
-    p = ALLOC_1D(p, 1);
+  p = ALLOC_1D(p, 1);
 
-    initialiseMesh(g);          // initialise 3D mesh
-    initialiseABC(g);           // initialise ABC
-    initialiseSlice(g);
-    initialiseSource(p, g);
+  initialiseABC(g);           // initialise ABC
+  initialiseSlice(g);
+  initialiseSource(p, g);
 
 
-    /* do time stepping */
-    for (g->time = 0; g->time < g->maxTime; g->time = g->time)
-    {
-        updateH(g);             // update magnetic fields in mesh
-        halfTimeStep(p, g);
-        updateE(g);             // update electric fields in mesh
-        //halfTimeStep(p, g);
-        sourceFunction(p, g);   // produce effects of source on local fields.
-        updateABC(g);           // apply ABCs
-        Slice(g);               // take a slice (if appropriate)
-    }                     // end of time-stepping
-    return 0;
+  /* do time stepping */
+  for (g->time = 0; g->time < g->maxTime; g->time = g->time)
+  {
+    updateH(g);             // update magnetic fields in mesh
+    halfTimeStep(p, g);
+    updateE(g);             // update electric fields in mesh
+    //halfTimeStep(p, g);
+    sourceFunction(p, g);   // produce effects of source on local fields.
+    updateABC(g);           // apply ABCs
+    Slice(g);               // take a slice (if appropriate)
+  }                     // end of time-stepping
+  return 0;
 }
