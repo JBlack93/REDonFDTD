45c5c29e40b045cfc054fca9a3bc707769cf4bfc
James Black <james.a.black@durham.ac.uk>
2020-03-04 19:38:56 +0000

    Fix: std::array initialisation

--- include/REDonFDTD/source.hpp
+++ include/REDonFDTD/source.hpp
@@ -8,33 +8,33 @@
 
 namespace REDonFDTD {
 
-//! @brief Particle struct contains all necessary information for updating the position of the particle.
+//! @brief Generic abstract virtual source class.
   class source {
   public:
     source();
     ~source() = default;
 
-/** @brief Calculate effect Particle has on neighbouring Mesh Points
- *  @param g        Mesh upon which the particle's effects are felt
+/** @brief Calculate effect source has on neighbouring Mesh Points
+ *  @param g        Mesh upon which the source's effects are felt
  */
     void sourceFunction(Mesh *g);
 
-/** @brief Update values of a particle at the end of a timestep
+/** @brief Update values of a source at the end of a timestep
  *  @param g        Mesh in which particle lives
  */
     virtual void timeAdvanceValues(Mesh *g) = 0;
 
   protected:
-    std::array<double,3> position{0,0,0};
-    std::array<double,3> velocity{0,0,0};
-    std::array<double,3> acceleration{0,0,0};
+    std::array<double,3> position;
+    std::array<double,3> velocity;
+    std::array<double,3> acceleration;
     std::array<int,6> coordinates;
 
 /** @brief Calculate the 6 points on the axes which give closest grid points
  */
     void findCell();
 
-/** @brief Calculate the E-Field produced by Particle on a particular point in S-T
+/** @brief Calculate the E-Field produced by source on a particular point in S-T
  *  @param g        Mesh upon which the particle's effects are felt
  *  @param x        x-position of grid point
  *  @param y        y-position of grid point
@@ -42,7 +42,7 @@ namespace REDonFDTD {
  */
     virtual std::array<double,3> eFieldProduced(Mesh *g, double x, double y, double z) = 0;
 
-/** @brief Calculate the B-Field produced by Particle on a particular point in S-T
+/** @brief Calculate the B-Field produced by source on a particular point in S-T
  *  @param g        Mesh upon which the particle's effects are felt
  *  @param eField   Corresponding Electric Field
  *  @param x        x-position of grid point
