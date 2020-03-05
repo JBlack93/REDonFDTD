e69842c637a5b0ecf4341212aeba48bf94df3b09
James Black <james.a.black@durham.ac.uk>
2020-03-05 23:18:56 +0000

    Placeholder options added to GUI window

--- REDonFDTDGUI/mainwindow.cpp
+++ REDonFDTDGUI/mainwindow.cpp
@@ -1,11 +1,20 @@
+#include <QMessageBox>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+
+, ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    for (int i = 0; i<100; ++i){
+        ui->comboBox_3->addItem("Step "+ QString::number(i+1));
+    }
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +22,24 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_pushButton_clicked(){
+    QString Message("This functionality is yet to be implemented\n");
+    QString Option1(ui->comboBox->currentText());
+    QString Option2(ui->comboBox_2->currentText());
+    QString Option3(ui->comboBox_3->currentText());
+    QString Option4(ui->comboBox_4->currentText());
+    Message.append("\nComponent Requested: ");
+    Message.append(Option1);
+    Message.append("\nPlane Requested: ");
+    Message.append(Option2);
+    Message.append("\nTimestamp Requested: ");
+    Message.append(Option3);
+    Message.append("\nPlotting : ");
+    Message.append(Option4);
+    QMessageBox::information(this, "Title",Message);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QMessageBox::information(this, "Title","This functionality is yet to be implemented\n");
+}
